9d5e0198890f6b4510e62746666bf08977eb592a
Tanmay Das <tanmaymishu@gmail.com>
2024-12-14 15:00:56 +0600

    Update tests

--- query.go
+++ query.go
@@ -2,66 +2,104 @@ package db
 
 import "github.com/huandu/go-sqlbuilder"
 
-func CreateBuilder(connName ...string) *sqlbuilder.CreateTableBuilder {
+type BuilderStruct struct {
+	*sqlbuilder.Struct
+}
+
+type BuilderCreateTable struct {
+	*sqlbuilder.CreateTableBuilder
+}
+
+type BuilderSelect struct {
+	*sqlbuilder.SelectBuilder
+}
+
+type BuilderInsert struct {
+	*sqlbuilder.InsertBuilder
+}
+
+type BuilderUpdate struct {
+	*sqlbuilder.UpdateBuilder
+}
+
+type BuilderDelete struct {
+	*sqlbuilder.DeleteBuilder
+}
+
+func StructBuilder(structValue interface{}, connName ...string) *BuilderStruct {
+	builder := sqlbuilder.NewStruct(structValue)
 	switch Get(connName...).Config.Driver {
 	case DialectSQLite:
-		return sqlbuilder.SQLite.NewCreateTableBuilder()
+		return &BuilderStruct{builder.For(sqlbuilder.SQLite)}
 	case DialectMySQL:
-		return sqlbuilder.MySQL.NewCreateTableBuilder()
+		return &BuilderStruct{builder.For(sqlbuilder.MySQL)}
 	case DialectPgSQL:
-		return sqlbuilder.PostgreSQL.NewCreateTableBuilder()
+		return &BuilderStruct{builder.For(sqlbuilder.PostgreSQL)}
 	default:
 		panic("unsupported driver")
 	}
 }
 
-func SelectBuilder(connName ...string) *sqlbuilder.SelectBuilder {
+func CreateTableBuilder(connName ...string) *BuilderCreateTable {
 	switch Get(connName...).Config.Driver {
 	case DialectSQLite:
-		return sqlbuilder.SQLite.NewSelectBuilder()
+		return &BuilderCreateTable{sqlbuilder.SQLite.NewCreateTableBuilder()}
 	case DialectMySQL:
-		return sqlbuilder.MySQL.NewSelectBuilder()
+		return &BuilderCreateTable{sqlbuilder.MySQL.NewCreateTableBuilder()}
 	case DialectPgSQL:
-		return sqlbuilder.PostgreSQL.NewSelectBuilder()
+		return &BuilderCreateTable{sqlbuilder.PostgreSQL.NewCreateTableBuilder()}
 	default:
 		panic("unsupported driver")
 	}
 }
 
-func InsertBuilder(connName ...string) *sqlbuilder.InsertBuilder {
+func SelectBuilder(connName ...string) *BuilderSelect {
 	switch Get(connName...).Config.Driver {
 	case DialectSQLite:
-		return sqlbuilder.SQLite.NewInsertBuilder()
+		return &BuilderSelect{sqlbuilder.SQLite.NewSelectBuilder()}
 	case DialectMySQL:
-		return sqlbuilder.MySQL.NewInsertBuilder()
+		return &BuilderSelect{sqlbuilder.MySQL.NewSelectBuilder()}
 	case DialectPgSQL:
-		return sqlbuilder.PostgreSQL.NewInsertBuilder()
+		return &BuilderSelect{sqlbuilder.PostgreSQL.NewSelectBuilder()}
 	default:
 		panic("unsupported driver")
 	}
 }
 
-func UpdateBuilder(connName ...string) *sqlbuilder.UpdateBuilder {
+func InsertBuilder(connName ...string) *BuilderInsert {
 	switch Get(connName...).Config.Driver {
 	case DialectSQLite:
-		return sqlbuilder.SQLite.NewUpdateBuilder()
+		return &BuilderInsert{sqlbuilder.SQLite.NewInsertBuilder()}
 	case DialectMySQL:
-		return sqlbuilder.MySQL.NewUpdateBuilder()
+		return &BuilderInsert{sqlbuilder.MySQL.NewInsertBuilder()}
 	case DialectPgSQL:
-		return sqlbuilder.PostgreSQL.NewUpdateBuilder()
+		return &BuilderInsert{sqlbuilder.PostgreSQL.NewInsertBuilder()}
 	default:
 		panic("unsupported driver")
 	}
 }
 
-func DeleteBuilder(connName ...string) *sqlbuilder.DeleteBuilder {
+func UpdateBuilder(connName ...string) *BuilderUpdate {
 	switch Get(connName...).Config.Driver {
 	case DialectSQLite:
-		return sqlbuilder.SQLite.NewDeleteBuilder()
+		return &BuilderUpdate{sqlbuilder.SQLite.NewUpdateBuilder()}
 	case DialectMySQL:
-		return sqlbuilder.MySQL.NewDeleteBuilder()
+		return &BuilderUpdate{sqlbuilder.MySQL.NewUpdateBuilder()}
 	case DialectPgSQL:
-		return sqlbuilder.PostgreSQL.NewDeleteBuilder()
+		return &BuilderUpdate{sqlbuilder.PostgreSQL.NewUpdateBuilder()}
+	default:
+		panic("unsupported driver")
+	}
+}
+
+func DeleteBuilder(connName ...string) *BuilderDelete {
+	switch Get(connName...).Config.Driver {
+	case DialectSQLite:
+		return &BuilderDelete{sqlbuilder.SQLite.NewDeleteBuilder()}
+	case DialectMySQL:
+		return &BuilderDelete{sqlbuilder.MySQL.NewDeleteBuilder()}
+	case DialectPgSQL:
+		return &BuilderDelete{sqlbuilder.PostgreSQL.NewDeleteBuilder()}
 	default:
 		panic("unsupported driver")
 	}
--- query_test.go
+++ query_test.go
@@ -1,39 +1,125 @@
 package db
 
-import "testing"
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func setupDb(dialect string) *sql.DB {
+	var config *Config
+
+	switch dialect {
+	case DialectSQLite:
+		config = &Config{
+			ConnName: "default",
+			Driver:   DialectSQLite,
+			Database: ":memory:",
+			Params:   "cache=shared",
+		}
+	case DialectMySQL:
+		{
+			port, _ := strconv.Atoi(os.Getenv("MYSQL_DB_PORT"))
+			config = &Config{
+				ConnName: "default",
+				Driver:   DialectMySQL,
+				Database: os.Getenv("MYSQL_DB_DATABASE"),
+				Host:     os.Getenv("MYSQL_DB_HOST"),
+				Port:     port,
+				User:     os.Getenv("MYSQL_DB_USER"),
+				Password: os.Getenv("MYSQL_DB_PASSWORD"),
+			}
+		}
+	case DialectPgSQL:
+		config = &Config{} // Not implemented yet
+	}
+
+	conn := NewConnection(config)
+	db := conn.Open()
+	DM().Add(config.ConnName, conn)
+	return db
+}
+
+func createUsersTable(t *testing.T, db *sql.DB) {
+	ctb := CreateTableBuilder().CreateTable("users")
+	ctb.Define("id", "INTEGER", "PRIMARY KEY")
+	ctb.Define("name", "VARCHAR(255)", "NOT NULL")
+	ctb.Define("created_at", "DATETIME", "NOT NULL")
+
+	q, _ := ctb.Build()
+
+	_, err := db.Exec(q)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+//func TestStruct(t *testing.T) {
+//	// TODO: Update this
+//	type User struct {
+//		ID uint64 `db:"id"`
+//	}
+//
+//	db := setupDb(DialectSQLite)
+//
+//	db.Exec(
+//		StructBuilder(&User{}).InsertInto("users").Build(),
+//	)
+//}
 
 func TestCreateTable(t *testing.T) {
-	// TODO: Update this
-	Get().Exec(
-		CreateBuilder().CreateTable("users").Build(),
-	)
+	db := setupDb(DialectSQLite)
+	createUsersTable(t, db)
 }
 
 func TestSelect(t *testing.T) {
-	// TODO: Update this
-	Get().Query(
-		SelectBuilder().Select("*").From("users").Build(),
-	)
+	db := setupDb(DialectSQLite)
+	createUsersTable(t, db)
+	sb, _ := SelectBuilder().Select("*").From("users").Build()
+	rows, err := db.Query(sb)
+	defer rows.Close()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
 }
 
 func TestInsert(t *testing.T) {
-	// TODO: Update this
-	Get().Exec(
-		InsertBuilder().InsertInto("users").Build(),
-	)
+	db := setupDb(DialectSQLite)
+	createUsersTable(t, db)
+
+	ib, args := InsertBuilder().InsertInto("users").
+		Cols("id", "name", "created_at").
+		Values(1, "Huan Du", 1234567890).
+		Values(2, "Charmy Liu", 1234567890).Build()
+
+	res, err := db.Exec(ib, args...)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if rowCount, err := res.RowsAffected(); err != nil || rowCount != 2 {
+		t.Errorf(err.Error())
+	}
 }
 
 func TestUpdate(t *testing.T) {
 	// TODO: Update this
+	db := setupDb(DialectSQLite)
+
 	ub := UpdateBuilder()
-	Get().Exec(
+	db.Exec(
 		ub.Update("users").Set(ub.Assign("foo", "bar")).Build(),
 	)
 }
 
 func TestDelete(t *testing.T) {
 	// TODO: Update this
-	Get().Exec(
+	db := setupDb(DialectSQLite)
+
+	db.Exec(
 		DeleteBuilder().DeleteFrom("users").Build(),
 	)
 }
